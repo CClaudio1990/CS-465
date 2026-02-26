6e6e8676857c0f4f53fd27abfc7f98bb83b4ab44
Christian Claudio <cclaudio21@gmail.com>
2026-02-26 13:14:19 -0500

    Add files via upload
    Module 6 Completed Baseline

---
+++ app_api/controllers/trips.js
@@ -0,0 +1,131 @@
+const mongoose = require('mongoose');
+const Trip = require('../models/travlr'); // Register model
+const Model = mongoose.model('trips');
+
+// GET: /trips - lists all trips
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+const tripsList = async(req, res) => {
+    const q = await Model
+        .find({}) // No filter, returns all records
+        .exec();
+
+        // Uncomment here to show
+        // console logs for query
+        // console.log(q);
+    
+    if(!q){ // Db returned no data
+        return res
+            .status(404)
+            .json(err);
+    } else { // Returns resulting trip list
+        return res
+            .status(200)
+            .json(q);
+    }
+};
+
+module.exports = {
+    tripsList
+};
+
+// GET: /trips/:tripCode - lists a single trip
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+const tripsFindByCode = async(req, res) => {
+    const q = await Model
+        .find({'code' : req.params.tripCode}) // Return a single record
+        .exec();
+
+        // Uncomment here to show
+        // console logs for query
+        // console.log(q);
+    
+    if(!q){ // Db returned no data
+        return res
+            .status(404)
+            .json(err);
+    } else { // Returns resulting trip list
+        return res
+            .status(200)
+            .json(q);
+    }
+};
+
+// POST: /trips - Adds a new trip
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+const tripsAddTrip = async(req, res) => {
+    const newTrip = new Trip({
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description
+    });
+
+    const q = await newTrip.save();
+
+        if(!q) { // Database returned no data
+            return res
+                .status(400)
+                .json(err);
+        } else {
+            return res
+                .status(201)
+                .json(q);
+        }
+
+        // Uncomment the following to show results on
+        // the console
+        // console.log(q);
+};
+
+// PUT: /trips/:tripCode - Adds a new Trip
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+const tripsUpdateTrip = async(req, res) => {
+    // Uncomment for debugging
+    console.log(req.params);
+    console.log(req.body);
+
+    const q = await Model
+        .findOneAndUpdate(
+            {'code' : req.params.tripCode},
+            {
+                code: req.body.code,
+                name: req.body.name,
+                length: req.body.length,
+                start: req.body.start,
+                resort: req.body.resort,
+                perPerson: req.body.perPerson,
+                image: req.body.image,
+                description: req.body.description
+            }
+        )
+        .exec();
+
+        if(!q) { // Database returned no data
+            return res
+                .status(400)
+                .json(err);
+        } else { // Return resulting updated trip
+            return res
+                .status(201)
+                .json(q);
+        }
+
+        // Uncomment the following line
+        // to show operation on console
+        // console.log(q);
+};
+
+module.exports = {
+    tripsList,
+    tripsFindByCode,
+    tripsAddTrip,
+    tripsUpdateTrip
+};
