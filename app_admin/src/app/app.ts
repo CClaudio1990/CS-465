126a183d7e656291ee613fdd37ee4d5fb75a0c15
Christian Claudio <cclaudio21@gmail.com>
2026-02-26 13:15:31 -0500

    Add files via upload
    Module 6 Completed Baseline

---
+++ app_admin/src/app/app.ts
@@ -0,0 +1,16 @@
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { TripListing } from './trip-listing/trip-listing';
+import { Navbar } from './navbar/navbar';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, TripListing, Navbar],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('Travlr Getaways Admin!');
+}
